b95c2fd4cb196c359b5bf8c170e52b184c92507c
Yakov <yayakobz@gmail.com>
2015-06-10 12:17:06 +0300

    Added vk sdk from cocoapods

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.3'
+
+target 'MadCat' do
+	pod 'VK-ios-sdk'
+end
---
+++ Pods/Target Support Files/Pods-MadCat/Pods-MadCat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MadCat : NSObject
+@end
+@implementation PodsDummy_Pods_MadCat
+@end
