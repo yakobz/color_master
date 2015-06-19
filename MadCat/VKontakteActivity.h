65bfca0403538ca2b8d08155905fb2e0c55c6c4e
Yakov <yayakobz@gmail.com>
2015-06-19 10:38:33 +0300

    Added sharing vk

---
+++ MadCat/VKontakteActivity.h
@@ -0,0 +1,17 @@
+//
+//  VKontakteActivity.h
+//  VKActivity
+//
+//  Created by Denivip Group on 28.01.14.
+//  Copyright (c) 2014 Denivip Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VKontakteActivity : UIActivity
+
+@property (nonatomic, copy) NSString *appID;
+
+- (id)initWithParent:(UIViewController*)parent;
+
+@end
